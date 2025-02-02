8d72e60aed33dda53f9cac1ce1581cde72bb8164
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-02-01 18:52:11 -0600

    Obtener paises por Continente en el servicio.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Region, Country, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   private _regions:Region[] = [Region.Africa,Region.Americas,Region.Asia,Region.Europe,Region.Oceania];
 
@@ -14,8 +20,15 @@ export class CountriesService {
     return [...this._regions];
   }
 
-  getCountriesByRegion(region: Region): SmallCountry[]{
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]>{
+    if(!region) return of([]);
 
-    return [];
+    const url:string = `${this.baseUrl}/region/${region}/?fields=cca3,name,borders`;
+
+    return  this.httpClient.get<SmallCountry[]>(url)
+    .pipe(
+      tap(resp => console.log(resp))
+    )
+
   }
 }
